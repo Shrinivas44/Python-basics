a201451a3a9a950043cc0025b44fdd64ca6c1512
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-02 11:36:22 +0530

    Create 02_input_output.py

---
+++ 02_input_output.py
@@ -0,0 +1,7 @@
+name = input("Enter your Name : ")
+age = int(input("Enter your Age : ")
+branch = input("Enter your Branch : ")
+
+print(f"Name = {name}")
+print(f"Age = {age}")
+print(f"Branch = {branch}")

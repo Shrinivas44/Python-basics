4b5c5b5ae4b598033c73794a0ca746e30ea8154d
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-02 11:45:57 +0530

    Create 03_data_types.py

---
+++ 03_data_types.py
@@ -0,0 +1,16 @@
+name = "Shrinivasa S"
+age = 21
+cgpa = 7.48
+passed = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(cgpa)
+print(type(cgpa))
+
+print(passed)
+print(type(passed))

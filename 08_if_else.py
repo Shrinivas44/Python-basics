a67e3d9385fc92f0253c002997f640e9cc429bad
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-02 19:09:43 +0530

    Update 08_if_else.py

--- 08_if_else.py
+++ 08_if_else.py
@@ -1,4 +1,4 @@
-#Example 1
+# Example 1 - Voting Eligibility
 age = int(input("Enter your age : "))
 if age >= 18:
   print("Eligible to Vote")
@@ -6,7 +6,7 @@ else:
   print("Not Eligible to vote")
 
 
-#Example 2
+# Example 2 - Even or Odd
 num = int(input("Enter a Number : "))
 if num%2 != 0:
   print("odd")

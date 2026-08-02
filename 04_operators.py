21282708f61548ce9bdcc22b9d1ad3a4610a7181
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-02 11:59:26 +0530

    Create 04_operators.py

---
+++ 04_operators.py
@@ -0,0 +1,10 @@
+a = 20
+b = 6
+
+print(f"Addition = {a+b}")
+print(f"Subtraction = {a-b}")
+print(f"Multiplication = {a*b}")
+print(f"Division = {a/b}")
+print(f"Modulus = {a%b}")
+print(f"Floor Division = {a//b}")
+print(f"Power = {a**b}")

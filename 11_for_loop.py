47cd7d1de93e410e459169274aae6a8d887f9d17
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-03 14:15:40 +0530

    Create 11_for_loop.py

---
+++ 11_for_loop.py
@@ -0,0 +1,16 @@
+#example 1:
+for i in range(5):
+  print("python")
+
+
+#Example 2:
+for i in range(5):
+  print(i)
+
+#Example 3 :Print numbers from 1 to 5.
+for i in range(5):
+  print(i+1)
+
+#Example 4 : print number from 10 to 15
+for i in range(10,16):
+  print(i)

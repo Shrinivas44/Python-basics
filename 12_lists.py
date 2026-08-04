fa0b093666ff6efba2eac4be30a4c391c45ebd16
Shrinivasa S <sshrinivasa744@gmail.com>
2026-08-04 14:58:28 +0530

    Create 12_lists.py

---
+++ 12_lists.py
@@ -0,0 +1,58 @@
+#Example 1: Create a List : Create a list containing the names of five fruits, Print the complete list
+fruit = ["Apple","Banana","Orange","Grapes","Mango"]
+print(fruit)
+
+'''Example 2: Access List Elements 
+Question:
+Create a list of five colors.
+Print:
+1. The first color
+2. The third color
+3. The fifth color'''
+colors = ["Red", "Black", "White", "Green", "Yellow"]
+print(colors[0])
+print(colors[2])
+print(colors[4])
+
+#Example 3: Modify a List Element
+fruit = ["Apple","Banana","Orange","Grapes","Mango"]
+fruit[1] == fruit[4]
+print(fruit)
+
+# Example 4: Add an Element to a List
+languages = ["Java","c","c++",]
+languages.append("python")
+print(languages)
+
+# Example 5: Remove an Element
+fruit = ["Apple","Banana","Orange","Grapes","Mango"]
+fruit.remove("Orange")
+print(fruit)
+
+# Example 6: Length of a List
+numbers = [1,2,3,4,5,6]
+print(len(numbers))
+
+# Example 7: Check if an Item Exists
+fruit = ["Apple","Banana","Orange","Grapes","Mango"]
+if "Mango" in fruit:
+  print("Mango found")
+else:
+  print("Mango not found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
